42310543212fb716ff7cd751ec5fc515f2ca54f4
Michael Oladoye <108640730+Jimike110@users.noreply.github.com>
2023-09-04 21:51:18 +0100

    Task 3. Object

---
+++ javascript-warm_up/12-object.js
@@ -0,0 +1,23 @@
+// Update this script to replace the value 12 with 89:
+// You are not allowed to use var
+
+// guillaume@ubuntu:~/$ cat 12-object.js
+// #!/usr/bin/node
+// const myObject = {
+//   type: 'object',
+//   value: 12
+// };
+// console.log(myObject);
+// /*
+// YOUR CODE HERE
+// */
+// console.log(myObject);
+
+#!/usr/bin/node
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.value = 89
+console.log(myObject);
