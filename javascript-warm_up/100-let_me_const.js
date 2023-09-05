928e004a35d67e8e84cff3ac7d1a5826bb1d936d
Michael Oladoye <108640730+Jimike110@users.noreply.github.com>
2023-09-05 08:33:55 +0100

    Task 5. Const or not const

---
+++ javascript-warm_up/100-let_me_const.js
@@ -0,0 +1,13 @@
+// Write a file that modifies the value of myVar to 333
+// guillaume@ubuntu:~/$ cat 100-main.js
+// #!/usr/bin/node
+// myVar = 89;
+// require('./100-let_me_const')
+// console.log(myVar);
+// guillaume@ubuntu:~/$ ./100-main.js
+// 333
+
+exports.modify = function () {
+    const myVar = 333;
+    return myVar;
+}
