59b06353247434ff32927ef7cf1c6622464f932a
Michael Oladoye <108640730+Jimike110@users.noreply.github.com>
2023-09-08 17:07:24 +0100

    Task 2. Rectangle #2

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -3,17 +3,12 @@ class Rectangle {
     constructor(w, h) {
         if (w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number') {
             // If w or h is equal to 0 or not a positive integer, create an empty object
-            this.width = undefined;
-            this.height = undefined;
-            return "Rectangle {}";
+            return {};
         }
         else {
             this.width = w;
             this.height = h;
         }
-    }
-    toString() {
-        return `Rectangle {}`;
     }
 }
 
