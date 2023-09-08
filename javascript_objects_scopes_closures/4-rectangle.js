91326c837388b2628d415f5a363b63827212ab51
Michael Oladoye <108640730+Jimike110@users.noreply.github.com>
2023-09-08 23:14:53 +0100

    Add module.exports line

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -28,6 +28,8 @@ class Rectangle {
     }
 }
 
+module.exports = Rectangle;
+
 // Write a class Rectangle that defines a rectangle:
 // You must use the class notation for defining your class
 // The constructor must take 2 arguments: w and h
