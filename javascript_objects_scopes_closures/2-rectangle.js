9434d1490d07b55b4539d3dd2ad4cf922ff27609
Michael Oladoye <108640730+Jimike110@users.noreply.github.com>
2023-09-08 14:10:13 +0100

    Task 2. Rectangle #2

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,10 +1,4 @@
-// Write a class Rectangle that defines a rectangle:
-// You must use the class notation for defining your class
-// The constructor must take 2 arguments w and h
-// Initialize the instance attribute width with the value of w
-// Initialize the instance attribute height with the value of h
-// If w or h is equal to 0 or not a positive integer, create an empty object
-
+#!/usr/bin/node
 class Rectangle {
     constructor (w, h) {
         this.width = w;
@@ -15,3 +9,13 @@ class Rectangle {
 if (w <= 0 || h <= 0) {
     Object.create({}, Rectangle);
 }
+
+module.exports = Rectangle;
+
+
+// Write a class Rectangle that defines a rectangle:
+// You must use the class notation for defining your class
+// The constructor must take 2 arguments w and h
+// Initialize the instance attribute width with the value of w
+// Initialize the instance attribute height with the value of h
+// If w or h is equal to 0 or not a positive integer, create an empty object
