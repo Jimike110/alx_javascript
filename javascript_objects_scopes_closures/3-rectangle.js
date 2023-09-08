33a5001c8c907460e80f5ef01d87d8c2bf8657d3
Michael Oladoye <108640730+Jimike110@users.noreply.github.com>
2023-09-08 21:20:47 +0100

    Task 3. Rectangle #3

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -8,6 +8,15 @@ class Rectangle {
     }
     this.width = w;
     this.height = h;
+    this.print = function () {
+      for (let i = 0; i < h; i++) {
+        console.log("X");
+        for (let j = 0; j < w - 1; j++) {
+          console.log("X");
+        }
+        console.log("\n");
+      }
+    };
   }
 }
 
@@ -15,7 +24,8 @@ module.exports = Rectangle;
 
 // Write a class Rectangle that defines a rectangle:
 // You must use the class notation for defining your class
-// The constructor must take 2 arguments w and h
+// The constructor must take 2 arguments: w and h
 // Initialize the instance attribute width with the value of w
 // Initialize the instance attribute height with the value of h
 // If w or h is equal to 0 or not a positive integer, create an empty object
+// Create an instance method called print() that prints the rectangle using the character X
