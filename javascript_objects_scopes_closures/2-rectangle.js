ff6398191e8f4df4b20f431fad49c92fed776c44
Michael Oladoye <108640730+Jimike110@users.noreply.github.com>
2023-09-08 21:18:44 +0100

    Task 3. Rectangle #3

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -2,20 +2,29 @@
 
 class Rectangle {
   constructor(w, h) {
-      if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
-          // Check if w or h is less than or equal to 0, or if they are not valid numbers (NaN)
-          return;
+    if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
+      return;
+    }
+    this.width = w;
+    this.height = h;
+    this.print = function () {
+      for (let i = 0; i < h; i++) {
+        console.log("X");
+        for (let j = 0; j < w - 1; j++) {
+          console.log("X");
+        }
+        console.log("\n");
       }
-      this.width = w;
-      this.height = h;
+    }
   }
 }
-  
+
 module.exports = Rectangle;
 
 // Write a class Rectangle that defines a rectangle:
 // You must use the class notation for defining your class
-// The constructor must take 2 arguments w and h
+// The constructor must take 2 arguments: w and h
 // Initialize the instance attribute width with the value of w
 // Initialize the instance attribute height with the value of h
 // If w or h is equal to 0 or not a positive integer, create an empty object
+// Create an instance method called print() that prints the rectangle using the character X
