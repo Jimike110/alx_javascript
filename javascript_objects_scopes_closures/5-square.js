18508c3bc9fd94f329caf93334085f3ae279e33e
Michael Oladoye <108640730+Jimike110@users.noreply.github.com>
2023-09-09 21:12:05 +0100

    Task 5. Square #0

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -8,7 +8,7 @@ class Square extends Rectangle {
     }
 }
 
-module.exports = Square;
+module.exports = Rectangle, Square;
 
 // Write a class Square that defines a square and inherits from Rectangle of 4-rectangle.js:
 // You must use the class notation for defining your class and extends
