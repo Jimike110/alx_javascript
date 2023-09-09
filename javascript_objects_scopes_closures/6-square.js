737eb91d3c6314b14d756277601326604df13062
Michael Oladoye <108640730+Jimike110@users.noreply.github.com>
2023-09-09 22:50:15 +0100

    Task 6. Square #1

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -3,6 +3,10 @@
 const Square5 = require('./5-square.js');
 
 class Square extends Square5 {
+    constructor(size) {
+        super(size);
+    }
+    
     charPrint(c) {
         if (c === undefined) {
             c = "X";
