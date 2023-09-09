c054297b1583ca92eb517d696b1375b7acbb30c1
Michael Oladoye <108640730+Jimike110@users.noreply.github.com>
2023-09-09 22:37:43 +0100

    Task 6. Square #1

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+const Square5 = require('./5-square.js');
+
+class Square extends Square5 {
+    charPrint(c) {
+        if (c === undefined) {
+            let c = "X";
+        } else { c = "c"}
+
+        for (let i = 0; i < this.size; i++) {
+            process.stdout.write(c);
+            for (let j = 1; j < this.size; j++) {
+                process.stdout.write(c)
+            }
+            console.log("");
+        }
+    }
+}
+
+module.exports = Square;
+
+// Write a class Square that defines a square and inherits from Square of 5-square.js:
+// You must use the class notation for defining your class and extends
+// Create an instance method called charPrint(c) that prints the rectangle using the character c
+// If c is undefined, use the character X
