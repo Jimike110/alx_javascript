cb3c7a080f08057ecd04f8235e8c3fb22122a48e
Jimike <oladoyemike@gmail.com>
2023-09-11 22:45:11 +0100

    1. Star wars movie title

--- javascript-network/1-starwars_title.js
+++ javascript-network/1-starwars_title.js
@@ -19,7 +19,7 @@ request.get(url, (error, body) => {
     const data = body;
     console.log(typeof body);
     // Print the title of the movie
-    console.log(data.body);
+    console.log(data.body.title);
   }
 });
 
