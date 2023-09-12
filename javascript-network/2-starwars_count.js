82fb9514c903c9fdcdffe05ba4977de317363104
Jimike <oladoyemike@gmail.com>
2023-09-12 20:55:12 +0100

    Task 2. Star wars Wedge Antilles

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -1,44 +1,37 @@
 #!/usr/bin/node
 
+// Import the request module
 const request = require("request");
 
-// Get the movie ID from the first argument
-const url = process.argv[2];
+// Define the API URL and the character ID
+const api_url = "https://swapi-api.alx-tools.com/api/films/";
+const character_id = 18;
 
-// Construct the URL with the endpoint and the ID
-const id = "18";
-
-// Make a GET request to the URL
-request.get(url, (error, body) => {
-  // If there is an error, print it
+// Use the request function to get the response from the API as a JSON object
+request(api_url, (error, response, body) => {
+  // Check for errors
   if (error) {
     console.error(error);
-  } else {
-    // Otherwise, parse the body as JSON
-    const data = body.body;
-
-    // Initialize the count variable
-    let count = 0;
-
-    // Loop through the data array
-    for (let i = 0; i < data.length; i++) {
-      // Get the characters array of the current movie
-      const characters = data[i].characters;
-      console.log(characters);
-      
-      // Loop through the characters array
-      for (let j = 0; j < characters.length; j++) {
-        // Check if the character ID is 18
-        if (characters[j].includes(id)) {
-          // Increment the count variable
-          count++;
-        }
-      }
-    }
-
-    // Print the count variable
-    console.log(count);
+    return;
   }
+
+  // Parse the body as a JSON object
+  const data = JSON.parse(body);
+
+  // Initialize a counter for the number of movies
+  let movie_count = 0;
+
+  // Loop through the results of the data
+  for (let result of data.results) {
+    // Check if the character ID is in the list of characters for each movie
+    if (result.characters.includes(String(character_id))) {
+      // Increment the movie count by one
+      movie_count++;
+    }
+  }
+
+  // Print the movie count
+  console.log(movie_count);
 });
 
 // Write a script that prints the number of movies where the character “Wedge Antilles” is present.
